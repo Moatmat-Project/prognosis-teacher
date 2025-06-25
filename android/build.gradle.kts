693937e4de36932007a09440241976c60700ab17
A B O U D <aboudkalaf10@gmail.com>
2025-06-25 14:01:57 +0300

    Temporary save before switching branches

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,10 +4,6 @@ allprojects {
     //         jvmTarget = "17"
     //     }
     // }    
-    repositories {
-        google()
-        mavenCentral()
-    }
 }
 
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
