09050169840a0f5862509ce1d032242d6b3d403e
Laeth234al <laeth2004alhalabe@gmail.com>
2025-07-03 20:04:53 +0300

    The End

--- lib/Presentation/tests/views/comment_managment_view.dart
+++ lib/Presentation/tests/views/comment_managment_view.dart
@@ -51,6 +51,8 @@ class _CommentsManagmentViewState extends State<CommentsManagmentView> {
               final replies = state.repliesMap[comment.id] ?? [];
               //
               return Card(
+                borderOnForeground: false,
+                shape: Border(),
                 child: Container(
                   decoration: BoxDecoration(
                     color: ColorsResources.onPrimary,
@@ -61,11 +63,11 @@ class _CommentsManagmentViewState extends State<CommentsManagmentView> {
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text(comment.username),
-                        IconButton(
-                          icon: Icon(Icons.delete, color: Colors.red),
+                        TextButton(
                           onPressed: () {
                             _confirmDeleteComment(context, comment.id);
                           },
+                          child: Text('حذف'),
                         ),
                       ],
                     ),
@@ -84,11 +86,11 @@ class _CommentsManagmentViewState extends State<CommentsManagmentView> {
                                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                 children: [
                                   Text(reply.username),
-                                  IconButton(
-                                    icon: Icon(Icons.delete, color: Colors.red),
+                                  TextButton(
                                     onPressed: () {
                                       _confirmDeleteReply(context, comment.id, reply.id);
                                     },
+                                    child: Text('حذف'),
                                   ),
                                 ],
                               ),
