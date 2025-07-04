2f6f43292f05e92dd461190f8b6979b0d34afdf9
A B O U D <aboudkalaf10@gmail.com>
2025-07-04 22:11:14 +0300

    enhance firebase mesaging handlers

--- lib/Features/notifications/data/handlers/firebase_messaging_handlers.dart
+++ lib/Features/notifications/data/handlers/firebase_messaging_handlers.dart
@@ -5,39 +5,43 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:moatmat_teacher/Core/injection/app_inj.dart';
+import 'package:moatmat_teacher/Core/resources/supabase_r.dart';
 import 'package:moatmat_teacher/Features/notifications/domain/requests/register_device_token_request.dart';
 import 'package:moatmat_teacher/Features/notifications/domain/usecases/display_firebase_notification_usecase.dart';
 import 'package:moatmat_teacher/Features/notifications/domain/usecases/register_device_token_usecase.dart';
 import 'package:moatmat_teacher/Presentation/notifications/state/notifications_bloc/notifications_bloc.dart';
 import 'package:moatmat_teacher/firebase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-
-///
-/// [firebase messaging background handler]
 @pragma('vm:entry-point')
 Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   debugPrint('A background message was received in flutter_background_service plugin: ${message.messageId}');
-
-  ///
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
   debugPrint('Firebase initialized');
 
-  ///
-  await initGetIt();
+  await Supabase.initialize(
+    url: SupabaseResources.url,
+    anonKey: SupabaseResources.key,
+  );
+  
+  
+  debugPrint('Supabase initialized');
 
-  ///
+  if (!locator.isRegistered<DisplayFirebaseNotificationUsecase>()) {
+    await initGetIt();
+  }
+
   await locator<DisplayFirebaseNotificationUsecase>().call(message: message);
-
   debugPrint('DisplayFirebaseNotificationUsecase called');
 }
 
 ///
 /// [notifications action handler]
 @pragma('vm:entry-point')
-void onDidReceiveBackgroundNotificationResponse(NotificationResponse? response) async {
+void onDidReceiveBackgroundNotificationResponse(
+    NotificationResponse? response) async {
   if (response == null) {
     return;
   }
